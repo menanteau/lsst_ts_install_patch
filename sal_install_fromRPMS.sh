995057b1c5bd1da0c8088fbf4275647c0b74c818
Felipe Menanteau <felipe@illinois.edu>
2019-05-14 15:29:04 -0500

    Update to use 3.9.2 rpms

--- sal_install_fromRPMS.sh
+++ sal_install_fromRPMS.sh
@@ -6,15 +6,15 @@ mv -v lsst-ts.repo /etc/yum.repos.d
 
 # These are the ones we've needed for now
 yum -y install OpenSpliceDDS-6.9.0
-yum -y install ATHeaderService-3.9.0
-yum -y install ATCamera-3.9.0
-yum -y install ATArchiver-3.9.0
-yum -y install EFD-3.9.0
-yum -y install Scheduler-3.9.0
-yum -y install ATPtg-3.9.0
-yum -y install ATMCS-3.9.0
-yum -y install ATSpectrograph-3.9.0
-yum -y install ATTCS-3.9.0
+yum -y install ATHeaderService-3.9.2
+yum -y install ATCamera-3.9.2
+yum -y install ATArchiver-3.9.2
+yum -y install EFD-3.9.2
+yum -y install Scheduler-3.9.2
+yum -y install ATPtg-3.9.2
+yum -y install ATMCS-3.9.2
+yum -y install ATSpectrograph-3.9.2
+yum -y install ATTCS-3.9.2
 # Add more CSC if needed
 
 # Get the setup conf
