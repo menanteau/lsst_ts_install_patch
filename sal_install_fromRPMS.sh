a05cbe68836598d009aaeff13ed7bcb50f7d1e7f
Felipe Menanteau <felipe@illinois.edu>
2019-05-28 15:36:58 -0500

    Updates to versio 3.10.0_001

--- sal_install_fromRPMS.sh
+++ sal_install_fromRPMS.sh
@@ -1,20 +1,26 @@
 # This is the recipe to install SAL/OpenSplice from rpms
 
+SAL_VERSION=3.10.0_001
+OPSL_VERSION=6.9.0
+
+# to remove
+# yum list installed | grep lsst-ts | awk '{printf "yum -y remove %s\n",$1}'
+
 # Add the lsst-ts repo
 wget https://lsst-web.ncsa.illinois.edu/~felipe/packages/lsst-ts.repo
 mv -v lsst-ts.repo /etc/yum.repos.d
 
 # These are the ones we've needed for now
-yum -y install OpenSpliceDDS-6.9.0
-yum -y install ATHeaderService-3.9.2
-yum -y install ATCamera-3.9.2
-yum -y install ATArchiver-3.9.2
-yum -y install EFD-3.9.2
-yum -y install Scheduler-3.9.2
-yum -y install ATPtg-3.9.2
-yum -y install ATMCS-3.9.2
-yum -y install ATSpectrograph-3.9.2
-yum -y install ATTCS-3.9.2
+yum -y install OpenSpliceDDS-$OPSL_VERSION
+yum -y install ATHeaderService-$SAL_VERSION
+yum -y install ATCamera-$SAL_VERSION
+yum -y install ATArchiver-$SAL_VERSION
+yum -y install EFD-$SAL_VERSION
+yum -y install Scheduler-$SAL_VERSION
+yum -y install ATPtg-$SAL_VERSION
+yum -y install ATMCS-$SAL_VERSION
+yum -y install ATSpectrograph-$SAL_VERSION
+yum -y install ATTCS-$SAL_VERSION
 # Add more CSC if needed
 
 # Get the setup conf
