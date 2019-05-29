1889a6949c6c6296224d5e0eb77804586ad5bd37
Felipe Menanteau <felipe@illinois.edu>
2019-05-29 17:24:50 -0500

    Update fitsio install script

--- install-fitsio.sh
+++ install-fitsio.sh
@@ -1,8 +1,21 @@
-wget https://github.com/menanteau/fitsio/archive/DMHSv0.1.tar.gz -O fitsio-DMHSv0.1.tar.gz 
-tar xfz fitsio-DMHSv0.1.tar.gz
-cd fitsio-DMHSv0.1
+#VERSION=1.0.3 -- this one does not work
+VERSION=1.0.0
+URL=https://github.com/esheldon/fitsio
+mkdir -p /tmp/fitsio-$USER
+cd /tmp/fitsio-$USER
+
+wget $URL/archive/v$VERSION.tar.gz -O fitsio-$VERSION.tar.gz
+tar xvfz fitsio-$VERSION.tar.gz
+cd fitsio-$VERSION
+
+# HOME install option
+#mkdir -p $HOME/Python3/lib64/python
+#export PYTHONPATH=$HOME/Python3/lib64/python:${PYTHONPATH}
+#python3 setup.py install --home=$HOME/Python3
+
+# /usr install
 python3 setup.py install --prefix=/usr
 
-#python setup.py install --prefix=/opt/rh/rh-python36/root/usr/
-#    rm -rf build && \
-#    python3.6 setup.py install --prefix=/usr
+# Clean up
+rm -rfv /tmp/fitsio-$USER
+
