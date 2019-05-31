cedcd1674cbb19f9e16f950ba2a91298f08ffc09
Felipe Menanteau <felipe@illinois.edu>
2019-05-31 12:06:12 -0500

    Update to fitsio

--- install-fitsio.sh
+++ install-fitsio.sh
@@ -1,5 +1,4 @@
-#VERSION=1.0.3 -- this one does not work
-VERSION=1.0.0
+VERSION=1.0.4
 URL=https://github.com/esheldon/fitsio
 mkdir -p /tmp/fitsio-$USER
 cd /tmp/fitsio-$USER
@@ -8,7 +7,7 @@ wget $URL/archive/v$VERSION.tar.gz -O fitsio-$VERSION.tar.gz
 tar xvfz fitsio-$VERSION.tar.gz
 cd fitsio-$VERSION
 
-# HOME install option
+# HOME install option -- for testing
 #mkdir -p $HOME/Python3/lib64/python
 #export PYTHONPATH=$HOME/Python3/lib64/python:${PYTHONPATH}
 #python3 setup.py install --home=$HOME/Python3
