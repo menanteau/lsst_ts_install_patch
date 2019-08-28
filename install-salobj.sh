d09268d9463c04a62184da4af38c4ccc0058d6f2
Felipe Menanteau <felipe@illinois.edu>
2019-08-28 15:05:38 -0500

    Update to fix minor bug

--- install-salobj.sh
+++ install-salobj.sh
@@ -1,14 +1,14 @@
+#!/bin/env bash
+
+set -e
+
 TS_SAL_VERSION=v3.10.0
 TS_XML_VERSION=v4.1.1
 INSTALL_PATH=/opt/lsst
 REPOS_TMP=/tmp/repos
 
 # Build from ts_sal/ts_xml from source using sal_install_fromSource.sh script
-mkdir -p /tmp/salbuild
-cp ./sal_install_fromSource.sh /tmp/salbuild
-cd /tmp/salbuild
 ./sal_install_fromSource.sh  -v_sal $TS_SAL_VERSION -v_xml $TS_XML_VERSION -p $INSTALL_PATH/sal-home
-rm -rf /tmp/salbuild
 
 TS_SALOBJ_VERSION=v4.1.0
 TS_IDL_VERSION=v0.2.0
