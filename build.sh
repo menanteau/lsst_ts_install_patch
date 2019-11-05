5efcaf8c5d2e2320b183e186822cd8a03e593317
Felipe Menanteau <felipe@illinois.edu>
2019-11-05 21:20:23 +0000

    Updates versions

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 export OSPL_VERSION=6.9.0
-export LSSTTS_SAL_VERSION=3.10.0
-export LSSTTS_SALOBJ_VERSION=4.5.0
-export LSSTTS_XML_VERSION=4.4.0
-export LSSTTS_IDL_VERSION=0.4.0
+export LSSTTS_SAL_VERSION=4.0.0
+export LSSTTS_SALOBJ_VERSION=5.0.0
+export LSSTTS_XML_VERSION=4.4.1
+export LSSTTS_IDL_VERSION=1.0.0
 export CCS_IDL_LIST="EFD ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod"
 
 ./install-salrpm.sh
