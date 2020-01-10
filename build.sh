f9b3baac3c3356d9168edb0e458e21a7b9f6636f
Felipe Menanteau <felipe@illinois.edu>
2020-01-10 12:02:08 -0600

    Updated to ts_xml 4.6.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 export OSPL_VERSION=6.9.0
 export LSSTTS_SAL_VERSION=4.0.0
 export LSSTTS_SALOBJ_VERSION=5.0.0
-export LSSTTS_XML_VERSION=4.4.1
+export LSSTTS_XML_VERSION=4.6.0
 export LSSTTS_IDL_VERSION=1.0.0
 export CCS_IDL_LIST="EFD ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod"
 
