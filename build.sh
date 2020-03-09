2e8b9f72869d6ff6eb8f1207b2f0f6fa98e0d9b4
Felipe Menanteau <felipe@illinois.edu>
2020-03-09 11:57:39 -0500

    Adding DIMM and Environment

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export LSSTTS_SAL_VERSION=4.0.0
 export LSSTTS_SALOBJ_VERSION=5.4.0
 export LSSTTS_XML_VERSION=4.7.0
 export LSSTTS_IDL_VERSION=1.1.0
-export CCS_IDL_LIST="EFD ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod ATDome DIMM CCArchiver CCCamera CCHeaderService"
+export CCS_IDL_LIST="EFD ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod ATDome DIMM CCArchiver CCCamera CCHeaderService DIMM Environment"
 export MINICONDA_PATH=/opt/miniconda3
 
 ./install-salrpm.sh
