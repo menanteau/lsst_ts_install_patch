6d40f74d17ce2af1f5e8ec1006ea8851f4d5ba16
Felipe Menanteau <felipe@illinois.edu>
2020-04-10 12:05:35 -0500

    Changes for xml 5.0.0 and salobj 5.8.0

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 export OSPL_VERSION=6.9.0
 export LSSTTS_SAL_VERSION=4.0.0
-export LSSTTS_SALOBJ_VERSION=5.4.0
-export LSSTTS_XML_VERSION=4.7.0
-export LSSTTS_IDL_VERSION=1.1.0
-export CCS_IDL_LIST="EFD ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod ATDome DIMM CCArchiver CCCamera CCHeaderService DIMM Environment"
+export LSSTTS_SALOBJ_VERSION=5.8.0
+export LSSTTS_XML_VERSION=5.0.0
+export LSSTTS_IDL_VERSION=1.1.3
+export CCS_IDL_LIST="ATHeaderService ATCamera ATArchiver ATPtg ATMCS ATSpectrograph ATTCS ATHexapod ATDome DIMM CCArchiver CCCamera CCHeaderService"
 export MINICONDA_PATH=/opt/miniconda3
 
 ./install-salrpm.sh
--- install-conda-base.sh
+++ install-conda-base.sh
@@ -11,7 +11,7 @@ source $MINICONDA_PATH/bin/activate
 conda config --add channels conda-forge
 
 # Pre-reqs for salobj/dds
-conda install -y pyyaml setuptools==41.0.1 jsonschema==3.0.1 Cython==0.29.12 boto3==1.11.14
+conda install -y pyyaml setuptools==41.0.1 jsonschema==3.0.1 Cython==0.29.12 boto3==1.11.14 moto=1.3.14
 
 # Pre-reqs for the HeaderService
 conda install -y fitsio==1.0.4 astropy ipython pyyaml
