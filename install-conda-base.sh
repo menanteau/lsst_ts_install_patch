27a94b30c676499ebf06f4dbab49300ee14ba7df
Felipe Menanteau <felipe@illinois.edu>
2020-04-24 14:01:45 -0500

    Updates for salobj 5.10.0

--- install-conda-base.sh
+++ install-conda-base.sh
@@ -14,5 +14,5 @@ conda config --add channels conda-forge
 conda install -y pyyaml setuptools==41.0.1 jsonschema==3.0.1 Cython==0.29.12 boto3==1.11.14 moto=1.3.14
 
 # Pre-reqs for the HeaderService
-conda install -y fitsio==1.0.4 astropy ipython pyyaml
+conda install -y fitsio==1.0.4 astropy=4.0.1 ipython pyyaml
 
