6bb2432fd9e7322d5d9dbb584b49f6ffcb7e9e4c
Felipe Menanteau <felipe@illinois.edu>
2020-06-05 12:24:37 -0500

    Update salobj version

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 export OSPL_VERSION=6.9.0
 export LSSTTS_SAL_VERSION=4.1.1
-export LSSTTS_SALOBJ_VERSION=5.10.0
+export LSSTTS_SALOBJ_VERSION=5.14.0
 export LSSTTS_XML_VERSION=5.1.0
 export LSSTTS_IDL_VERSION=1.2.0
 #export CCS_IDL_LIST="ATHeaderService ATAOS ATCamera ATArchiver ATPtg MTPtg ATMCS ATSpectrograph Hexapod ATHexapod ATDome DIMM CCArchiver CCCamera CCHeaderService"
