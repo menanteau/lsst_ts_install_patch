770d353882ff0ab3af31d18b63c910e998197b85
Felipe Menanteau <felipe@illinois.edu>
2020-06-22 09:48:35 -0500

    Update to versions

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@ set -e
 
 export OSPL_VERSION=6.9.0
 export LSSTTS_SAL_VERSION=4.1.1
-export LSSTTS_SALOBJ_VERSION=5.14.0
-export LSSTTS_XML_VERSION=5.1.0
-export LSSTTS_IDL_VERSION=1.2.0
-#export CCS_IDL_LIST="ATHeaderService ATAOS ATCamera ATArchiver ATPtg MTPtg ATMCS ATSpectrograph Hexapod ATHexapod ATDome DIMM CCArchiver CCCamera CCHeaderService"
+export LSSTTS_SALOBJ_VERSION=5.15.0
+export LSSTTS_XML_VERSION=6.0.0
+export LSSTTS_IDL_VERSION=1.2.2
 export MINICONDA_PATH=/opt/miniconda3
 export INSTALL_PATH=/opt/lsst
 export HEADERSERVICE_VERSION=2.3.0
