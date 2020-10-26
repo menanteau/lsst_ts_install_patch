af4d52b05b672157776cca60064546c35fc7ae5e
Felipe Menanteau <felipe@illinois.edu>
2020-10-26 16:02:20 -0500

    Updates for new SAL

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 set -e
 
 export OSPL_VERSION=6.10.4
-export OSPL_RPM_VERSION=6.10.4-5.el7
-export LSSTTS_DDS_VERSION=6.10.4_2 
-export LSSTTS_SAL_VERSION=5.0.0
-export LSSTTS_XML_VERSION=6.1.0
-export LSSTTS_SALOBJ_VERSION=6.0.2
-export LSSTTS_IDL_VERSION=2.0.0
+export OSPL_RPM_VERSION=6.10.4-6.el7
+export LSSTTS_DDS_VERSION=6.10.4_6 
+export LSSTTS_SAL_VERSION=5.0.1
+export LSSTTS_XML_VERSION=6.2.1
+export LSSTTS_SALOBJ_VERSION=6.0.3
+export LSSTTS_IDL_VERSION=2.1.0
 export LSSTTS_IDL_CONDA_VERSION=${LSSTTS_IDL_VERSION}_${LSSTTS_XML_VERSION}_${LSSTTS_SAL_VERSION}
-export HEADERSERVICE_VERSION=2.3.0
+export HEADERSERVICE_VERSION=2.5.0
 export MINICONDA_PATH=/opt/miniconda3
 export INSTALL_PATH=/opt/lsst
 export TAG=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_xml_v${LSSTTS_XML_VERSION}
@@ -17,4 +17,4 @@ export TAG=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_xml_v${LSS
 ./install-salrun.sh
 ./install-salidl.sh
 ./install-salobj.sh
-./install-headerserice.sh
+./install-headerservice.sh
--- install-salidl.sh
+++ install-salidl.sh
@@ -5,7 +5,7 @@ set -e
 # This will install ts-idl and also it will
 # install all of the sal_revCoded_*.idl files in $MINICONDA_PATH/lib/python3.7/idl
 source ${MINICONDA_PATH}/bin/activate
-conda install -y -c lsstts python=3.7 ts-idl=${LSSTTS_IDL_CONDA_VERSION}
+conda install -y -c lsstts -c lsstts/label/dev python=3.7 ts-idl=${LSSTTS_IDL_CONDA_VERSION}
 echo "------------------------------------------"
 echo "  Installed ts_idl: ${LSSTTS_IDL_CONDA_VERSION}"
 echo "------------------------------------------"
