c761c1d8dfc31f5efd1b49419224b5301994651b
Felipe Menanteau <felipe@illinois.edu>
2021-03-07 21:24:12 -0600

    Update for HS

--- install-headerservice.sh
+++ install-headerservice.sh
@@ -3,9 +3,10 @@
 
 set -e
 source ${MINICONDA_PATH}/bin/activate
+conda install -y python=3.8 fitsio pyyaml
 #conda install -y fitsio==1.0.4 ipython pyyaml
 #conda install -y -c lsstts python=3.7 headerservice=${HEADERSERVICE_VERSION}
-conda install -y -c lsstts python=3.8 headerservice=${HEADERSERVICE_VERSION}
+#conda install -y -c lsstts python=3.8 headerservice=${HEADERSERVICE_VERSION}
 #echo "------------------------------------------"
 #echo "  Installed headerservice: ${HEADERSERVICE_VERSION}"
 #echo "------------------------------------------"
