fbc86acd90b25163eca5b71e415e0157b6e5b93c
Felipe Menanteau <felipe@illinois.edu>
2021-06-22 09:43:28 -0500

    Updates for Cycle 21

--- install-salrun.sh
+++ install-salrun.sh
@@ -44,6 +44,7 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh
 source $MINICONDA_PATH/bin/activate
 conda config --add channels conda-forge
 # Install python dds using conda
+conda install -y astropy=4.1
 conda install -y -c lsstts -c lsstts/label/dev python=3.8 ts-dds==$LSSTTS_DDS_VERSION
 echo "------------------------------------------"
 echo "  Installed ts_idl: ${LSSTTS_DDS_VERSION} "
