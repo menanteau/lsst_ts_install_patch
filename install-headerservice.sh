437888bca95dc8388fd39b79e39e601fa9556eda
Felipe Menanteau <felipe@illinois.edu>
2022-01-25 11:54:19 -0600

    Update to headerservice installation

--- install-headerservice.sh
+++ install-headerservice.sh
@@ -4,9 +4,18 @@
 set -e
 source ${MINICONDA_PATH}/bin/activate
 conda install -y python=3.8 fitsio pyyaml
-#conda install -y fitsio==1.0.4 ipython pyyaml
-#conda install -y -c lsstts python=3.7 headerservice=${HEADERSERVICE_VERSION}
-conda install -y -c lsstts python=3.8 headerservice=${HEADERSERVICE_VERSION}
+
+# We need to go old school and use a python install instead
+source ${MINICONDA_PATH}/bin/activate && \
+mkdir -p /tmp/repos && \
+cd /tmp/repos && \
+git clone https://github.com/lsst-dm/HeaderService -b ${HEADERSERVICE_VERSION} && \
+cd HeaderService && \
+export PYTHONPATH=$PYTHONPATH:$CONDA_PREFIX/HeaderService/python &&\
+python setup.py install --prefix=$CONDA_PREFIX/HeaderService --install-lib=$CONDA_PREFIX/HeaderService/python --install-data $CONDA_PREFIX/HeaderService
+cd ../
+rm -rf /tmp/repos
+
 echo "------------------------------------------"
 echo "  Installed headerservice: ${HEADERSERVICE_VERSION}"
 echo "------------------------------------------"
